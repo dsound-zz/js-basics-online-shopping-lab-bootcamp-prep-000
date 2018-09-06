99d43ad2eeb1722fc6894f1a2eafbba10fe6fb3c
demian sims <demiansims@gmail.com>
2018-09-06 02:10:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function viewCart() {
 
      
 function total() {
-  var numbers = getCart()[getCart().itemPrice]
+  var numbers = cart.itemPrice
   var sum;
   for (let i=0; i < numbers.length; i++) {
     sum += numbers[i];
