9588fd2502cef366b6b432f208019dfd9197c09a
demian sims <demiansims@gmail.com>
2018-09-06 02:58:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,5 +62,14 @@ function removeFromCart(name) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+ if (!cardNumber) {
+   return "Sorry, we don't have a credit card on file for you." }
+   else {
+    var totalCart = cart.total()
+    cart = [];
+    return `Your total cost is $${totalCart}, which will be charged to the card ${cardNumber}.`
+     
+   }
+   
+ 
 }
